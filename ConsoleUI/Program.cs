1e4285dcba435b9f9a0f3062f0fb08fb29685f5e
Emre Değirmenci <emred1999@hotmail.com>
2022-01-25 19:12:35 +0300

    Database connection added.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,17 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(20,100))
             {
                 Console.WriteLine(product.ProductName);
             }
