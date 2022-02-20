f58ee3a858a0fcac8166f22f2e5527df8772aa5e
Emre Değirmenci <emred1999@hotmail.com>
2022-02-20 03:27:09 +0300

    Autofac, FluentValidation and AOP added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,9 +31,11 @@ namespace WebAPI
         {
             // Autofac, Ninject, CastleWindsor, StructureMap, LighInject, DryInject --> IoC Container
             // AOP
+            // Postsharp
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
