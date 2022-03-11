116cf691185c6413aec5a8ac84f03d2fae7f172e
Emre Değirmenci <emred1999@hotmail.com>
2022-03-11 22:09:16 +0300

    CORS mechanism added.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
@@ -27,7 +28,8 @@ namespace WebAPI.Controllers
 
         [HttpGet("getall")]
         public IActionResult GetAll()
-        {          
+        {
+            Thread.Sleep(1000);
             var result = _productService.GetAll();
             if (result.Success)
             {
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,6 +45,7 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -74,6 +75,9 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
 
             app.UseHttpsRedirection();
 
